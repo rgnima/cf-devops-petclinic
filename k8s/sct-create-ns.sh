9dc599166d2e3901e7b5a37312f677a537a4d89c
Ricardo Nima <ricardonima@gmail.com>
2025-05-06 18:37:54 -0500

    deploy secret

--- k8s/sct-create-ns.sh
+++ k8s/sct-create-ns.sh
@@ -5,8 +5,8 @@ else
    kubectl create namespace app
 fi
 
-if kubectl get namespace prometheus > /dev/null 2>&1; then
-   echo "prometheus Namespace already exists."
-else
-   kubectl create namespace prometheus
-fi
+#if kubectl get namespace prometheus > /dev/null 2>&1; then
+#   echo "prometheus Namespace already exists."
+#else
+#   kubectl create namespace prometheus
+#fi
